e6ae04bf2b7828eaec2b6d3ea8431bd82d780ea9
Renata Brasil <renatabbss@gmail.com>
2021-09-07 22:19:58 -0300

    Fixing tests

--- seller-management/src/test/java/br/com/ninja/controllers/test/AdministratorControllerTest.java
+++ seller-management/src/test/java/br/com/ninja/controllers/test/AdministratorControllerTest.java
@@ -1,4 +1,15 @@
 package br.com.ninja.controllers.test;
 
-public class AdministratorControllerTest {
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class AdministratorControllerTests {
+
+    @Test
+    public void testEssaPorra() {
+        assertEquals(1, 1);
+    }
 }
