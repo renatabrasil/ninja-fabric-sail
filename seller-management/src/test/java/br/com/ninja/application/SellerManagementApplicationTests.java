a47fb589f6c1427250eee946000350375d35d4cb
Renata Brasil <renatabbss@gmail.com>
2021-09-13 22:49:47 -0300

    Configurando o gradle para os subprojetos

--- seller-management/src/test/java/br/com/ninja/application/SellerManagementApplicationTests.java
+++ seller-management/src/test/java/br/com/ninja/application/SellerManagementApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.ninja.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SellerManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
--- seller-management/src/test/java/br/com/ninja/controllers/test/AdministratorControllerTests.java
+++ seller-management/src/test/java/br/com/ninja/controllers/test/AdministratorControllerTests.java
@@ -1,9 +1,9 @@
 package br.com.ninja.controllers.test;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class AdministratorControllerTests {
